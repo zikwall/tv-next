34a43cffcf1a3ef7648c7eba486d7d76b5d6a5b2
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-12-02 16:03:01 +0300

    Test only one fetch. OK

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function About() {
     return (
-            <div>
-                <p>This is the about page</p>
-            </div>
+        <div>
+            <p>This is the about page</p>
+
+            <Link href="/">
+                <a>Home</a>
+            </Link>
+        </div>
     );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import Head from 'next/head'
 import { ChannelContex } from "../app/context";
+import Link from "next/link";
 
 const Home = () => {
     const channels = useContext(ChannelContex);
@@ -11,6 +12,10 @@ const Home = () => {
                 <title>Просто ТВ</title>
                 <link rel="icon" href="/favicon.ico"/>
             </Head>
+
+            <Link href="/about">
+                <a>About</a>
+            </Link>
         </>
     );
 };
