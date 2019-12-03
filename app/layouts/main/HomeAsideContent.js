06e06fe70dcdbac51d80090997403290c477cf0e
Andreev Andrey <andrey.kapitonov.96@gmail.com>
2019-12-03 16:36:12 +0300

    Review Menu

--- app/layouts/main/HomeAsideContent.js
+++ app/layouts/main/HomeAsideContent.js
@@ -1,30 +1,23 @@
 import React from "react";
-import Link from "next/link";
+import { Menu, MenuActiveItem } from "../../components/ui/menu";
 
 const HomeAsideContent = () => {
     return (
         <>
-            <div className="col-md-12">
-                <div className="footer-about">
-                    <div className="widget">
-                        <h5 className="widget-title">Navigation</h5>
-                        <ul className="list-inline vertical-list">
-                            <li><Link href="/">
-                                <a>Go to Home</a>
-                            </Link></li>
-                            <li><Link href="/help">
-                                <a>Help &amp; Support</a>
-                            </Link></li>
-                            <li><Link href="/contact">
-                                <a>Contact</a>
-                            </Link></li>
-                            <li><Link href="/privacy">
-                                <a>Terms &amp; Privacy</a>
-                            </Link></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
+            <Menu>
+                <MenuActiveItem isActive url="/" >
+                    Go to Home
+                </MenuActiveItem>
+                <MenuActiveItem url="/help" >
+                    Help &amp; Support
+                </MenuActiveItem>
+                <MenuActiveItem url="/contact" >
+                    Contact
+                </MenuActiveItem>
+                <MenuActiveItem url="/privacy" >
+                    Terms &amp; Privacy
+                </MenuActiveItem>
+            </Menu>
         </>
     )
 };
