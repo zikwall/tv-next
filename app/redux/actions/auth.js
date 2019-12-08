3c05f91034c82e0a779346f9822f6fd3b5cb170c
zikwall <andrey.kapitonov.96@gmail.com>
2019-12-08 17:02:33 +0300

    :art: Review

--- app/redux/actions/auth.js
+++ app/redux/actions/auth.js
@@ -46,7 +46,6 @@ const deauthenticate = () => {
     };
 };
 
-
 export {
     authenticate,
     reauthenticate,
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from "next/dist/next-server/lib/head";
+import Head from "next/head";
 import { connect } from "react-redux";
 import React from "react";
 import Slider from "react-slick";
